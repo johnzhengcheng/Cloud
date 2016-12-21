da51b1610ad3928169a76f753e8c79d8b112d4da
johnzhengcheng <13602763471@139.com>
2016-12-21 15:36:25 +0800

    myClass

---
+++ myClass.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a mathematics script file.
+Which is used for deriving the difference of each function.
+"""
+
+'''
+Created on Dec 17,2016
+
+@author: Zheng Cheng
+'''
+
+class dataInput():
+	def myInput():
+		print("Please input your string")
+		maths_exp=input()
+		return(maths_exp)
