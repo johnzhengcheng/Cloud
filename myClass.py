a700ee05bede0cb611dd92c0c4637a6a4c62e486
johnzhengcheng <13602763471@139.com>
2016-12-22 16:27:25 +0800

    myClass

--- myClass.py
+++ myClass.py
@@ -11,9 +11,50 @@ Created on Dec 17,2016
 
 @author: Zheng Cheng
 '''
+'''
+使用方法
+t=myClass.myData('t')
+t.myInput()
+t.myOutput()
+'''
 
-class dataInput():
-	def myInput():
-		print("Please input your string")
-		maths_exp=input()
-		return(maths_exp)
+from sympy import *
+a,b,m,n,x, y,z,t,k= symbols('a b m n x y z t k')
+init_printing(use_unicode=True)
+
+
+class myData:    
+    def __init__(self,tag):
+        self.title=tag
+        self.maths_exp=""
+        self.maths_deriv=""
+        self.maths_integrate=""
+        self.maths_output=""
+        
+    def myInput(self):
+        print("Please Input your string")
+        self.maths_exp=input()
+        
+    def myOutput(self):
+        pprint(self.maths_output,use_unicode=False)
+        
+    def myValue(self,value):
+        self.maths_output=value
+        
+    
+            
+        
+        
+  
+  
+
+      
+      
+  
+	
+    
+  
+  
+
+  
+   
