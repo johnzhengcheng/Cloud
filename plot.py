57e376dd3acbbc79fe78b47f36c319e76778ccc6
johnzhengcheng <13602763471@139.com>
2017-01-13 15:46:21 +0800

    plot.py

--- plot.py
+++ plot.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 x = np.arange(0, 5, 0.1);
 y = x**2+2*x+1 
-plt.plot(x, y)
+plt.plot(x, y,linestyle='-.',color='red')
 x=np.array([1,2,3])
 y=x**2+2*x+1
-plt.scatter(x,y,c='red',linewidths=5)
+plt.scatter(x,y,linewidths=5)
 plt.show()
