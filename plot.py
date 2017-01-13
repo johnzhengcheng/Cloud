c8bd331319df7bf7c5a9e38af323b7e163a3f91a
johnzhengcheng <13602763471@139.com>
2017-01-13 17:11:50 +0800

    annotate

--- plot.py
+++ plot.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 x = np.arange(0, 5, 0.1);
 y = x**2+2*x+1 
 plt.plot(x, y,linestyle='-.',color='red')
-x=np.array([1,2,3])
+x=np.array([1,2,3,4,5])
 y=x**2+2*x+1
 plt.scatter(x,y,linewidths=5)
+plt.annotate('Point 1 ',xy=(1,4),arrowprops=dict(facecolor='blue', shrink=0.05))  
+plt.annotate('Point 5 ',xy=(5,5**2+2*5+1),xytext=(4.96,31.4),arrowprops=dict(facecolor='blue', shrink=0.05))
+
 plt.show()
